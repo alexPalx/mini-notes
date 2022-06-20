94b0077a35496c4298c2ef0c3f287c15004bcf2d
Alex Palx <alex.teanow@gmail.com>
2022-06-20 14:43:01 +0300

    feat: implement list item editing function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const editButtons = document.querySelectorAll('.button_edit');
 const deleteButtons = document.querySelectorAll('.button_delete');
 const copyButtons = document.querySelectorAll('.button_copy');
 
+editButtons.forEach(button => button.addEventListener('click', event => switchEditSave(event.target.parentNode.parentNode.children[0], event.target)));
 deleteButtons.forEach(button => button.addEventListener('click', event => removeItem(event.target.parentNode.parentNode)));
 copyButtons.forEach(button => button.addEventListener('click', event => updateClipboard(event.target.parentNode.parentNode.children[0].textContent)));
 
@@ -12,4 +14,9 @@ function updateClipboard(text) {
     navigator.clipboard.writeText(text)
         .then(() => console.log(`"${text}" copied!`),
               () => console.log('Error'));
+}
+
+function switchEditSave(content, button) {
+    content.contentEditable = !JSON.parse(content.contentEditable);
+    button.textContent = JSON.parse(content.contentEditable) ? 'Save' : 'Edit';
 }
