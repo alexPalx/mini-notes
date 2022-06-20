748bec96b3af65bfb694221c82626dfa19f4bfd3
Alex Palx <alex.teanow@gmail.com>
2022-06-20 15:19:04 +0300

    feat: implement function to add list item

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 const editButtons = document.querySelectorAll('.button_edit');
 const deleteButtons = document.querySelectorAll('.button_delete');
 const copyButtons = document.querySelectorAll('.button_copy');
+const createButtons = document.querySelectorAll('.button_create');
 
 editButtons.forEach(button => button.addEventListener('click', event => switchEditSave(event.target.parentNode.parentNode.children[0], event.target)));
 deleteButtons.forEach(button => button.addEventListener('click', event => removeItem(event.target.parentNode.parentNode)));
 copyButtons.forEach(button => button.addEventListener('click', event => updateClipboard(event.target.parentNode.parentNode.children[0].textContent)));
+createButtons.forEach(button => button.addEventListener('click', event => addItem(event.target.parentNode.parentNode)));
+
+function addItem(listNode) {
+    const itemId = listNode.children.length - 1;
+    const itemText = listNode.children[itemId].children[0].textContent;
+
+    const template = `
+    <div class="item__text" contentEditable="false">${itemText}</div>
+    <div class="item__buttons">
+        <button class="button button_edit">Edit</button>
+        <button class="button button_delete">Del</button>
+        <button class="button button_copy">Copy</button>
+    </div>`;
+
+    const newElem = document.createElement('li');
+    newElem.classList.add('content__item', 'item', `#${itemId}`);
+    newElem.innerHTML = template;
+
+    // listeners for buttons: edit, delete, copy
+    newElem.children[1].children[0].addEventListener('click', event => switchEditSave(event.target.parentNode.parentNode.children[0], event.target));
+    newElem.children[1].children[1].addEventListener('click', event => removeItem(event.target.parentNode.parentNode));
+    newElem.children[1].children[2].addEventListener('click', event => updateClipboard(event.target.parentNode.parentNode.children[0].textContent));
+
+    listNode.children[itemId].children[0].textContent = '';
+    listNode.insertBefore(newElem, listNode.children[itemId]);
+}
 
 function removeItem(node) {
     node.remove();
@@ -13,7 +40,7 @@ function removeItem(node) {
 function updateClipboard(text) {
     navigator.clipboard.writeText(text)
         .then(() => console.log(`"${text}" copied!`),
-              () => console.log('Error'));
+            () => console.log('Error'));
 }
 
 function switchEditSave(content, button) {
